7fd17bbc60ecdf1b084b5843b9ca1c75e1992222
Leonardo <leonardoffbs381@gmail.com>
2026-03-12 12:28:13 -0300

    mais comandos

--- script.js
+++ script.js
@@ -6,8 +6,12 @@
 
 //var resultado = num1 + num2;//cria variavel do resultado
 //console.log("a soma de " + num1 + " e " + num2 + " é de = " + resultado)//mostra o resultado no console (sempre colocar "+" antes e depois de cada varial, salvo na ultima)
-//window.alert("o seu resultado é = " + resultado)//mostra o resultado
+//alert("o seu resultado é = " + resultado)//mostra o resultado
 
+var nomeAlunoEtec = "Leonardo Menezes";//string
+var idadeAluno = 16;//numero inteiro
+var alturaAluno = 1.76;//float
+var estudanteAluno = true;//boolean
 
 
 
